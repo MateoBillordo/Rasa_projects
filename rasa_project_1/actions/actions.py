84200d6fc4bcbe37b2be2977f9506e21aeb03fc6
unknown <billordomateo02@gmail.com>
2022-08-27 02:04:10 -0300

    Creacion del repo

---
+++ rasa_project_1/actions/actions.py
@@ -0,0 +1,37 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from email import message
+from turtle import st
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_precios"
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            costo_producto = tracker.latest_message['entities'][0]['value']
+            message = "El costo del producto es de "
+            if str(costo_producto) == "tarjeta de credito":
+                message += "$500 por mes"
+            elif str(costo_producto) == "caja de ahorro":
+                message += "$0"
+            elif str(costo_producto) == "cuenta corriente":
+                message += "$1200 por mes"
+            elif str(costo_producto) == "caja fuerte":
+                message += "$6500 cada tres meses"
+            else:
+                message = "El producto indicado no se encuentra disponible"
+            dispatcher.utter_message(text=str(message))
+            return []
