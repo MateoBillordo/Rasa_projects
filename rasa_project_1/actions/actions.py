b5ae747b9a4f1aaaece9cb7b997af2dda22cb903
MateoBillordo <billordomateo02@gmail.com>
2022-08-27 04:29:02 -0300

    Adicion de slot

--- rasa_project_1/actions/actions.py
+++ rasa_project_1/actions/actions.py
@@ -11,6 +11,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
 
 class ActionHelloWorld(Action):
@@ -29,7 +30,32 @@ class ActionHelloWorld(Action):
                 message += "$1200 por mes"
             elif str(costo_producto) == "caja fuerte":
                 message += "$6500 cada tres meses"
+            elif str(costo_producto) == "seguro multirriesgo":
+                message += "$40000 al mes (cubre hasta 5 personas)"
             else:
                 message = "El producto indicado no se encuentra disponible"
             dispatcher.utter_message(text=str(message))
+            return []
+
+class ActionProductosEmpresa(Action):
+
+    def name(self) -> Text:
+        return "action_productos_empresa"
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            intent = tracker.latest_message['intent'].get('name')
+            dispatcher.utter_message(text="Entiendo, en ese caso, en que puedo ayudarte?")
+            if str(intent) == "productos_empresas":
+                return [SlotSet("empresa","true")]
+            return []
+
+class ActionProductosEmpresaDisponibles(Action):
+
+    def name(self) -> Text:
+        return "action_productos_empresa_disponibles"
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            empresa = tracker.get_slot("empresa")
+            if str(empresa) == "true":
+                dispatcher.utter_message(text="Los productos disponibles son seguros multirriesgos")
             return []
