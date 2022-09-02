015a911a2298da4cad99d4d1901952862de45aa1
MateoBillordo <billordomateo02@gmail.com>
2022-09-01 21:16:43 -0300

    Creacion de version2 del bot

--- rasa_project_1/actions/actions.py
+++ rasa_project_1/actions/actions.py
@@ -17,45 +17,9 @@ from rasa_sdk.events import SlotSet
 class ActionHelloWorld(Action):
 
     def name(self) -> Text:
-        return "action_precios"
+        return "hello_world"
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            costo_producto = tracker.latest_message['entities'][0]['value']
-            message = "El costo del producto es de "
-            if str(costo_producto) == "tarjeta de credito":
-                message += "$500 por mes"
-            elif str(costo_producto) == "caja de ahorro":
-                message += "$0"
-            elif str(costo_producto) == "cuenta corriente":
-                message += "$1200 por mes"
-            elif str(costo_producto) == "caja fuerte":
-                message += "$6500 cada tres meses"
-            elif str(costo_producto) == "seguro multirriesgo":
-                message += "$40000 al mes (cubre hasta 5 personas)"
-            else:
-                message = "El producto indicado no se encuentra disponible"
-            dispatcher.utter_message(text=str(message))
+            dispatcher.utter_message(text="Hello World")
             return []
 
-class ActionProductosEmpresa(Action):
-
-    def name(self) -> Text:
-        return "action_productos_empresa"
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            intent = tracker.latest_message['intent'].get('name')
-            dispatcher.utter_message(text="Entiendo, en ese caso, en que puedo ayudarte?")
-            if str(intent) == "productos_empresas":
-                return [SlotSet("empresa","true")]
-            return []
-
-class ActionProductosEmpresaDisponibles(Action):
-
-    def name(self) -> Text:
-        return "action_productos_empresa_disponibles"
-    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-            empresa = tracker.get_slot("empresa")
-            if str(empresa) == "true":
-                dispatcher.utter_message(text="Los productos disponibles son seguros multirriesgos")
-            return []
