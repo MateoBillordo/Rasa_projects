e5fb96065d18b31eb821a912f720f59fe66560ec
MateoBillordo <82384308+MateoBillordo@users.noreply.github.com>
2022-09-10 15:16:33 -0300

    Modificacion y testeo

--- yo_bot/actions/actions.py
+++ yo_bot/actions/actions.py
@@ -7,21 +7,70 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from email import message
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class ActionResponderCuantos(Action):
+
+    def name(self) -> Text:
+        return "action_responder_cuantos"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        elemento = tracker.latest_message['entities'][0]['value']
+        message = ""
+
+        if str(elemento) == "materias":
+            message += "X cantidad"
+        elif str(elemento) == "finales":
+            message += "X cantidad"
+        elif str(elemento) == "optativas":
+            message += "X cantidad"
+        else:
+            message += "X cantidad"
+        dispatcher.utter_message(text=str(message))
+
+        return []
+
+class ActionResponderQueOCuales(Action):
+
+    def name(self) -> Text:
+        return "action_responder_que_o_cuales"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        elemento = tracker.latest_message['entities'][0]['value']
+        message = ""
+
+        if str(elemento) == "materias":
+            message += "Lista de "+str(elemento)
+        elif str(elemento) == "finales":
+            message += "Lista de "+str(elemento)
+        elif str(elemento) == "optativas":
+            message += "Lista de "+str(elemento)
+        else:
+            message += "Lista de "+str(elemento)
+        dispatcher.utter_message(text=str(message))
+
+        return []
