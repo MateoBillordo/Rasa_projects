58cdcc1d58b925062c0f851e136d9cd9972a5e5a
MateoBillordo <82384308+MateoBillordo@users.noreply.github.com>
2022-09-22 11:23:57 -0300

    Modificacion de actions

--- yo_bot/actions/actions.py
+++ yo_bot/actions/actions.py
@@ -9,6 +9,7 @@
 
 from email import message
 from typing import Any, Text, Dict, List
+from unittest import result
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -29,6 +30,23 @@ class ActionHelloWorld(Action):
 
         return []
 
+class ConsultarAProlog():
+
+    def consulta(self,motivo) -> List[Text]:
+        result = []
+        with PrologMQI() as mqi:
+            with mqi.create_thread() as prolog_thread:
+                prolog_thread.query_async(r"consult('C:\\Rasa_projects\\Rasa_projects\\yo_bot\\data\\datos_academicos.pl')", find_all=False)
+                
+                if str(motivo) == "cursadas":
+                    prolog_thread.query_async("materiasCursadas(X)", find_all=False)
+                elif str(motivo) == "finales":
+                    prolog_thread.query_async("materiasAprobadas(X)",find_all=False)
+
+                result = prolog_thread.query_async_result()[0]['X'] #obtengo la lista de prolog
+
+        return result
+
 class ActionResponderCuantos(Action):
 
     def name(self) -> Text:
@@ -38,28 +56,15 @@ class ActionResponderCuantos(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        elemento = tracker.latest_message['entities'][0]['value']
         message = ""
+        motivoConsulta = tracker.latest_message['entities'][0]['value']
+        result = ConsultarAProlog.consulta(motivoConsulta)
+        tamanio = len(result)
 
-        with PrologMQI() as mqi:
-            with mqi.create_thread() as prolog_thread:
-                prolog_thread.query_async(r"consult('C:\\Rasa_projects\\Rasa_projects\\yo_bot\\data\\datos_academicos.pl')", find_all=False)
-                if str(elemento) == "materias":
-                    prolog_thread.query_async("materiasCursadas(X)", find_all=False)
-                    result = prolog_thread.query_async_result()[0]['X'] #obtengo la lista de prolog
-                    
-                    message = ""
-
-                elif str(elemento) == "finales":
-                    prolog_thread.query_async("materiasAprobadas(X)", find_all=False)
-                    result = prolog_thread.query_async_result()
-                    message = str(len(result))
-
-                elif str(elemento) == "optativas":
-                    message += "Cant de "+str(elemento)
-
-                else:
-                    message = "Perdon, no te entendi"
+        if tamanio > 0:
+            message += tamanio
+        else:
+            message = "Perdon, no te entendi"
 
         dispatcher.utter_message(text=str(message))
 
@@ -74,37 +79,20 @@ class ActionResponderQueOCuales(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        elemento = tracker.latest_message['entities'][0]['value']
-
         message = ""
-        with PrologMQI() as mqi:
-            with mqi.create_thread() as prolog_thread:
-                #la unica forma que se me ocurre de diferenciar las entidades es por su valor. Implica una cadena muy grande de if-else
-                prolog_thread.query_async("consult('plan_de_estudios.pl')", find_all=False)
-                if str(elemento) == "materias":
-                    #message += "Lista de "+str(elemento)
-                    prolog_thread.query_async("materiasCursadas(X)", find_all=False)
-                    result = prolog_thread.query_async_result()
-                    for valor in result.values():
-                        message += str(valor) + " "
+        motivoConsulta = tracker.latest_message['entities'][0]['value']
+        result = ConsultarAProlog.consulta(motivoConsulta)
+        tamanio = len(result)
 
-                elif str(elemento) == "finales":
-                    # message += "Lista de "+str(elemento)
-                    prolog_thread.query_async("materiasAprobadas(X)", find_all=False)
-                    result = prolog_thread.query_async_result()
-                    for valor in result.values():
-                        message += str(valor) + " "
-
-                elif str(elemento) == "optativas":
-                    #prolog_thread.query_async("", find_all=False)
-                    message += "Lista de "+str(elemento)
-
-                elif str(elemento) == "carrera":
-                    message = "Ingenieria de Sistemas"
-
-
-                else:
-                    message = "Perdon, no te entendi"
+        if tamanio == 1:
+            message = result[0]
+        elif tamanio > 1:
+            for i in range(0,tamanio-1):
+                message += result[i] + ', '
+            message = message.rstrip(', ')
+            message += ' y ' + result[tamanio-1]
+        else:
+            message = "Perdon, no te entendi"
 
         dispatcher.utter_message(text=str(message))
 
--- yo_bot/actions/prueba.py
+++ yo_bot/actions/prueba.py
@@ -1,24 +1,27 @@
-from swiplserver import PrologMQI, PrologThread
+# from dataclasses import replace
+# from unittest import result
+# from swiplserver import PrologMQI, PrologThread
 
-elemento = "materias"
-message = ""
+# elemento = "materias"
+# message = ""
 
-# formatearRespuesta(input):
+# # formatearRespuesta(input):
 
 
-with PrologMQI() as mqi:
-    with mqi.create_thread() as prolog_thread:
-        #la unica forma que se me ocurre de diferenciar las entidades es por su valor. Implica una cadena muy grande de if-else
-        prolog_thread.query_async(r"consult('C:\\Rasa_projects\\Rasa_projects\\yo_bot\\data\\datos_academicos.pl')", find_all=False)
+# with PrologMQI() as mqi:
+#     with mqi.create_thread() as prolog_thread:
+#         #la unica forma que se me ocurre de diferenciar las entidades es por su valor. Implica una cadena muy grande de if-else
+#         prolog_thread.query_async(r"consult('C:\\Rasa_projects\\Rasa_projects\\yo_bot\\data\\datos_academicos.pl')", find_all=False)
             
-        prolog_thread.query_async("materiasAprobadas(X)", find_all=False)
-        result = prolog_thread.query_async_result()[0]['X'] #obtengo la lista de prolog
-        
-        print(result)
-        print()
-        for i in range(len(result)-1):
-            message += result[i] + ', '
-        
-        message += "y " + result[len(result)-1]
+#         prolog_thread.query_async("materiasAprobadas(X)", find_all=False)
+#         # result = prolog_thread.query_async_result()[0]['X'] #obtengo la lista de prolog
+#         result = ['hola','chau','nos vemos']
 
-        print(message)
+#         for i in range(len(result)-1):
+#             message += result[i] + ', '
+        
+#         message = message.rstrip(', ')
+
+#         message += ' y ' + result[len(result)-1]
+
+#         print(message)
