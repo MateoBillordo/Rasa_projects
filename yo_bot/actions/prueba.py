cd55ff957c9bbd743307ae21b84708cbb8d79119
MateoBillordo <82384308+MateoBillordo@users.noreply.github.com>
2022-10-25 01:29:39 -0300

    Modificación

--- yo_bot/actions/prueba.py
+++ yo_bot/actions/prueba.py
@@ -1,4 +1,6 @@
 # from textblob import TextBlob
 
-# texto="me gusto muchisimo"
+# texto="me robaron la notebook"
+# print("")
+# print(texto)
 # print(TextBlob(str(texto)).translate(from_lang="es", to="en").sentiment)
